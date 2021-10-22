ce819e5e496184bd2bb1383dd98c2ec4a05094a6
SuFei-lab <91953024+SuFei-lab@users.noreply.github.com>
2021-10-22 15:00:28 +0800

    Update SAM.R

--- R/SAM.R
+++ R/SAM.R
@@ -11,20 +11,19 @@
 #' @examples SAM(datacase,datacontrol,p=0.05)
 SAM<-function(datacase,datacontrol,p){
   library(siggenes)
-      gene_name<-data.frame(datacase[,1])
-      colnames(gene_name)[1]<-"mir_name"
-      case<-data.frame(datacase[,-1])
-      control<-data.frame(datacontrol[,-1])
-      Feature_selection_set<-as.matrix(cbind(case,control))
-      cl<-c(rep(1,dim(case)[2]),rep(0,dim(control)[2]))
-      sam.out<-siggenes::sam(as.matrix(Feature_selection_set),cl,gene.names = gene_name[,1])
-      sum.sam.out<-summary(sam.out,0.000000000000001)
-      sam_mat_sig<-sum.sam.out@mat.sig
-      sam_pvalue_mirname<-data.frame(rownames(sam_mat_sig)[sam_mat_sig$rawp<=p])
-
-      Feature_selection_set<-cbind.data.frame(gene_name,Feature_selection_set)
-      select<-Feature_selection_set[match(sam_pvalue_mirname[,1],Feature_selection_set[,1]),]
-      select<-select[complete.cases(select),]
+     gene_name<-data.frame(datacase[,1])
+    colnames(gene_name)[1]<-"mir_name"
+    case<-data.frame(datacase[,-1])
+    control<-data.frame(datacontrol[,-1])
+    Feature_selection_set<-as.matrix(cbind(case,control))
+    cl<-c(rep(1,dim(case)[2]),rep(0,dim(control)[2]))
+    sam.out<-siggenes::sam(as.matrix(Feature_selection_set),cl,gene.names = gene_name[,1])
+    p_value<-data.frame(mirname=gene_name,p.value=sam.out@p.value)
+    p_value<-p_value[p_value$p.value<=0.05,]
+  
+    Feature_selection_set<-cbind.data.frame(gene_name,Feature_selection_set)
+    select<-Feature_selection_set[match(p_value$mir_name,Feature_selection_set[,1]),]
+    select<-select[complete.cases(select),]
 
       return(select)
 
